8d5c2f1e63578b70f1e19cd60ef7f39ef7c2ddd4
Dominic Cassidy <dominic@koroutine.tech>
2025-08-07 16:53:41 +0100

    feat: Add config and hyperswarm

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 #!/usr/bin/env node
 
 const readline = require('readline')
+const cenc = require('compact-encoding')
+const b4a = require('b4a')
 const { PunchGit } = require('./lib/punch.js')
+const { repoConfig } = require('./lib/messages.js')
 
 // const GIT_PUNCH_SERVER_NAMESPACE = 'git-remote-punch'
 
@@ -11,8 +14,10 @@ const argv = process.argv.slice(0)
 // args[1] == git-remote-punch location
 const remote = argv[2]
 const url = argv[3]
-// const url = argv[3]
-// const config = decodeUrl(url)
+
+// Get the config from the url
+const configBuffer = b4a.from(url.replace('punch://', ''), 'hex')
+const config = cenc.decode(repoConfig, configBuffer) || {}
 
 const capabilities = () => {
   process.stdout.write('option\nfetch\npush\nlist\n\n')
@@ -25,7 +30,7 @@ const main = async (args) => {
 
   const punch = new PunchGit({
     remote,
-    url
+    ...config
   })
 
   await punch.ready()
