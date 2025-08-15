368540d9a24490b1000ed35919ec2ad45eeb72c9
Dominic Cassidy <dominic@koroutine.tech>
2025-08-15 16:13:45 +0100

    chore: use node for git-remote-punch

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
-#!/usr/bin/env bare
-
-/* globals Bare */
-const process = require('process');
-
+#!/usr/bin/env node
 const run = require("pear-run")
 
-const ARGV = global.Bare.argv.slice(1)
-const punchGitArgs = global.Bare.argv.slice(2)
+const ARGV = process.argv.slice(1)
+const punchGitArgs = process.argv.slice(2)
 
 class API {
     static RUNTIME = "pear"
@@ -15,13 +11,10 @@ class API {
     app = {}
   }
 
-const runtimeDir = global.Bare.argv[1].split("/").slice(0, -1).join("/")
-// TODO: replace to pear url
+// TODO: add an arg to run locally
+// const runtimeDir = global.Bare.argv[1].split("/").slice(0, -1).join("/")
 const link = "pear://cgnph3qsrfk55pcpzyd3ab7rheqd9jjcxfam3ypmu9989q1xk3zy"
-global.Bare.argv.length = 1
-global.Bare.argv.push('run', link)
 global.Pear = new API()
-
 
 // ? run direct and let it stdout/stderr itself?
 run(link, punchGitArgs)
